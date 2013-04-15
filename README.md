3532310b97aa1091155ddda75e5da38b4285deb7
uberger500 <uberger500@gmail.com>
2013-04-15 08:58:39 -0400

    done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This assignment is hard!
+1. All aspects correctly implemented, I think. The table headers in the response don't quite fit the date but
+that is cosmetic.
+2. I had a short discussion with Foster Lockwood about the architecture of a web application.
+3. Way too many hours, thinking there was not quite enough instruction. I spent a week trying to figure out what Node.js is and getting lost in way too many different languages and add-ons. For actual coding, I probably 
+spent 30-40 hours.
+
--- scorecenterapi.js
+++ scorecenterapi.js
@@ -3,7 +3,6 @@ var app = express(express.logger());
 app.use(express.bodyParser());
 app.set('title', 'nodeapp');
 
-
 // Mongo initialization
 var mongoUri = process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
@@ -13,18 +12,12 @@ var mongo = require('mongodb');
 
 var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
 	db = databaseConnection;
-        	if(!error) {
-                    console.log("We are connected");
-//                    var collection = db.collection('scorecenter');
-  //                var doc1 = {'hello':'doc1'};
-    //                collection.insert(doc1);
-                    }
-        });
+        if(!error) {
+            console.log("We are connected");
+        }
+    });
 
-//var mongoUri = process.env.MONGOLAB_URL 
-//MONGOLAB_URI: mongodb://heroku_app14958216:5bijf3ts652u91o3glilooi633@ds037077.mongolab.com:37077/heroku_app14958216
-
-
+//CORS
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -33,35 +26,27 @@ app.all('*', function(req, res, next) {
 });
 
 app.post('/submit.json', function(req, res) {
-    var score = req.body;
-    console.log('Adding score: ' + JSON.stringify(score));
     db.collection('scorecenter', function(err, collection) {
         collection.insert({game_title: req.body.game_title, username: req.body.username, score: req.body.score, created_at:new Date()}, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred'});
             } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
                 res.send(result[0]);
             }
         });
     }); 
 });
 
-app.get('/submit.json', function(req, res) {
-    res.set('Content-Type', 'text/html');
-    res.send('name submitted1');
-    });
-
 app.get('/', function (request, response) {
     db.collection('scorecenter', function(err, collection) {
         collection.find().toArray(function(err, items) {
-
+        //put results in table body
         var tableBody = "";
 	    for (i=0; i < items.length; i++) {
 	        tableRow = "<tr>" + "<td>" + items[i].game_title + "<td/>"+"<td>" +items[i].username+"<td/>"+"<td>"+items[i].score+"<td/>"+"<td>"+items[i].created_at+"<td/>"+"<tr/>";
 	        tableBody += tableRow;
 	        }       
-        
+        //add table header to table body
         var table = "<table>"+"<tr>"+"<th>"+"Game_Title" + "</th>"+"<th>"+"Username"+"</th>"+"<th>"+"Score"+"</th>"+"<th>"+"      Created_On"+"</th>"+"<tr/>"+ tableBody + "</table>";
         response.set('Content-Type', 'text/html');
         response.send(table);
@@ -71,9 +56,12 @@ app.get('/', function (request, response) {
 
 app.get('/highscores.json', function (request, response) {
     db.collection('scorecenter', function(err, collection) {
-        collection.find({game_title:'frogger'},{'limit':10,'sort':{'score':-1}}).toArray(function(err, items) {
+       collection.find(request.query,{'limit':10,'sort':{'score':-1}}).toArray(function(err, items) {
+        if (items.length > 0) {
         response.set('Content-Type', 'text/json');
         response.send(JSON.stringify(items));
+        } else { 
+        response.send('game title not found');}
         });
     });
 });
@@ -89,12 +77,10 @@ app.post('/usersearch', function(request, response) {
 			if (items.length == 1) {response.set('Content-Type', 'text/html');
 			var result = "<table>"+"<tr>"+"<th>"+"Game_Title" + "</th>"+"<th>"+"Username"+"</th>"+"<th>"+"Score"+"</th>"+"<th>"+"Created_On"+"</th>"+"<tr/>" +"<tr>"+"<td>"+items[0].game_title + "</td>"+"<td>"+items[0].username+"</td>"+"<td>"+items[0].score+"</td>"+"<td>"+items[0].created_at+"</td>"+"<tr/>" + "</table>";
 			response.send(result);}
-			else if (items.length > 1) {
-			
-			
+			else if (items.length > 1) {		
 			var tops = []; //put biggest scores into separate array
 			tops[0] = items[0];
-			for (i = 1; i < (items.length); i++) {
+			for (i = 1; i < (items.length); i++) {//iterate over items and put unique game titles in tops
 			    if(items[i].game_title != items[0].game_title) {
 			        for (j = 0; j < tops.length; ) {
 			        if(items[i].game_title == tops[j].game_title) {break;}
@@ -109,13 +95,13 @@ app.post('/usersearch', function(request, response) {
 				    }
 			    }
             }
-            
+            //place results in table object
             var tableBody = "";
 	        for (i=0; i < tops.length; i++) {
 	        tableRow = "<tr>" + "<td>" + tops[i].game_title + "<td/>"+"<td>" +tops[i].username+"<td/>"+"<td>"+tops[i].score+"<td/>"+"<td>"+tops[i].created_at+"<td/>"+"<tr/>";
 	        tableBody += tableRow;
 	        }       
-        
+            //table header
             var table = "<table>"+"<tr>"+"<th>"+"Game_Title" + "</th>"+"<th>"+"Username"+"</th>"+"<th>"+"Score"+"</th>"+"<th>"+"      Created_On"+"</th>"+"<tr/>"+ tableBody + "</table>";
             response.set('Content-Type', 'text/html');
             response.send(table);
@@ -131,5 +117,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-//response.send(200,'<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"><title>Submit username</title></head><body><h1>Search for user:</h1><input type="text" id="name" name="name" size="30" /></body></html>');
